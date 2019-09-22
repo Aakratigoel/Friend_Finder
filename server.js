20d45c6240ff0f9b3327bd7b700d75d1e7b6c5a6
Aakrati Goel <aakratigoel@gmail.com>
2019-09-21 20:26:27 -0700

    changes in server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var app = express();
+var PORT = process.env.PORT || 3700
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 var htmlroute = require('./app/routing/htmlRoutes.js');
@@ -9,4 +10,4 @@ var apiroute = require('./app/routing/apiRoutes.js');
 app.use('/apiroutes', apiroute);
 // //both index.js and things.js should be in same directory
  //app.use('/things', things);
-app.listen(3700);
+app.listen(PORT);
